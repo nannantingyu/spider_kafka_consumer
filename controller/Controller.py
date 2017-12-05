8a723185ca8806a1d5d7a5d0773801e28f0b227f
yangmingming <yangmingming@91guoxin.com>
2017-12-05 10:22:07 +0800

    consumer改为多地址配置

--- controller/Controller.py
+++ controller/Controller.py
@@ -30,4 +30,4 @@ class Controller(object):
         self.post_sn_url = settings.post_address
         self.token = '9286168e06a110cd374caa0f67f08199'
 
-        self.consumer = KafkaConsumer(topic, bootstrap_servers=self.server['host'], group_id='spider')
+        self.consumer = KafkaConsumer(topic, bootstrap_servers=self.server['host'], group_id='spider', auto_offset_reset="earliest")
